87bc87d894ea7f88e5f9a439673de4431ae65143
César Freitas <190026090@aluno.unb.br>
2023-03-08 10:57:34 -0300

    update

--- tse_eleicoes_estados_novo.py
+++ tse_eleicoes_estados_novo.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import basedosdados as bd
 import numpy as np
-from funs_tse import extract_data, candidatos, tabela, partidos, partidos2, tabela_filiacao
+from funs_tse import extract_data, candidatos, tabela, partidos, tabela_filiacao
 
 lista_uf =['SP']
     
@@ -9,6 +9,6 @@ for uf in lista_uf:
     df_eleicao = extract_data(f"{uf}")
     candidatos_df = candidatos(df_eleicao, f"'{uf}'", "2022")
     tabela(candidatos_df, f'{uf}', '2022')
-    df_partidos = partidos2(candidatos_df, f"'{uf}'")
+    df_partidos = partidos(candidatos_df, f"'{uf}'")
     tabela_filiacao(df_partidos, f"{uf}", "2022")
     
